169772087ebbb2bcdf3d7a1b5921f5cc44a59e05
amicuswat <amicuswat@gmail.com>
2022-11-07 08:47:22 +0300

    optimize query for most_popular_posts

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': post.comments__count,
+        'comments_amount': post.comments_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -48,10 +48,18 @@ def index(request):
 
     # most_popular_posts = []  # TODO. Как это посчитать?
 
-    posts = Post.objects.annotate(Count('likes', distinct=True), Count('comments', distinct=True)).order_by('-likes__count').prefetch_related('author') #.prefetch_related('tags')
+    posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count').prefetch_related('author') #.prefetch_related('tags')
     most_popular_posts = posts[:5]
+    most_pupular_posts_is = [post.id for post in most_popular_posts]
 
-    fresh_posts = Post.objects.order_by('published_at').annotate(Count('comments', distinct=True)).prefetch_related('author') #.prefetch_related('tags')
+    posts_with_comments = Post.objects.filter(id__in=most_pupular_posts_is).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
+
+    fresh_posts = Post.objects.order_by('published_at').annotate(comments_count=Count('comments')).prefetch_related('author') #.prefetch_related('tags')
     most_fresh_posts = list(fresh_posts)[-5:]
 
     tags = Tag.objects.annotate(num_posts=Count('posts')).order_by('-num_posts')
